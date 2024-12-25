32cb65f4e1aed9fe924a6c0ca5671b8f41794cd7
Pugina-Maria <mashapugina0708@gmail.com>
2024-12-25 08:30:15 +0500

    Add files via upload

---
+++ Maria.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+using namespace std;
+int n;
+const double PI = 3.141592653589793;
+double x, s;
+double factorial(int n)
+{
+	if (n == 1)//условие выхода из рекурсии
+		return 1;
+	return factorial(n - 1) * n;
+}
+double rec(double x, int n)
+{
+	if (n == 0) //условие выхода из рекурсии
+		return x;
+	return ((cos(n * PI / 4)) / factorial(n) * pow(x, n)) + rec(x, n - 1);
+}
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите натуральное значение n и любое значение x через пробел ";
+	cin >> n >> x;
+	while (n < 1)
+	{
+		cout << "Вы ввели не натуральное значение n " << endl;
+		cout << "Введите натуральное значение n и любое значение x через пробел ";
+		cin >> n >> x;
+	}
+	s = rec(x, n - 1);
+	cout << "S = " << s << endl;
+	return 0;
+}
---
+++ mariaFib.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+int n, i, c;
+int a = 0;
+int b = 1;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    cout << "Введите значение n, 2 и более";// Ввод количества чисел последовательности
+    cin >> n;
+    n = n - 2;
+    cout << "Последовательность Фибоначчи:" << endl;
+    cout << a << endl;// Первый член последовательности Фибоначчи
+    cout << b << endl;// Второй член последовательности Фибоначчи
+    for (i = 0; i < n; i++)
+    {
+        c = a + b;
+        cout << c << "" << endl;// Вывод остальных членов последовательности Фибоначчи
+        a = b;
+        b = c;
+    }
+    return 0;
+}
